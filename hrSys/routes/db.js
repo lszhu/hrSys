e467430dd707bca0e0e71cb2cb63191574a1141c
Jason Zhu <jasonzhu.dev@gmail.com>
2014-05-19 15:06:18 +0800

    add db.js to access mongoDB

---
+++ hrSys/routes/db.js
@@ -0,0 +1,92 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+mongoose.connect(
+    'mongodb://localhost:27017/hrsys',
+    {
+        server: {socketOptions: {keepAlive: 1}},
+        user: 'hrsys',
+        pass: 'letmein'
+        //auth: {authenticationDatabase: 'hrsys'}
+    }
+);
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+    console.error('connection error:', err);
+});
+db.once('open', function() {
+    console.log('database connected.');
+});
+
+var itemSchema = new Schema({
+    username: String,
+    idNumber: String,
+    nation: String,
+    marriage: Boolean,
+    education: Number,
+    phone: String,
+    address: String,
+    insurance: [Number],
+    employment: Number,
+    workTrend: Boolean,
+    jobPreference: Number,
+    workPlace: [Number],
+    jobType: [Number],
+    industry: [Number],
+    project: Number,
+    salary: Number,
+    workExperience: Number,
+    technicalGrade: Number,
+    trainingStatus: Number,
+    postTraining: String,
+    preferredTraining: [Number],
+    editor: String,
+    auditor: String,
+    modifiedDate: Date
+});
+
+var Item = mongoose.model('hrmsg', itemSchema);
+
+// used to create test data
+var newItem = new Item({
+    username: 'test',
+    idNumber: '987655442398765544',
+    nation: '维吾尔族',
+    marriage: true,
+    education: 3,
+    phone: '13912345678',
+    address: '中国湖南长沙岳麓山',
+    insurance: [0, 1, 3, 4],
+    employment: 2,
+    workTrend: true,
+    jobPreference: 2,
+    workPlace: [0, 1, 2, 5],
+    auditor: '张三',
+    modifiedDate: new Date()
+});
+
+exports.save = function(hrMsg) {
+    Item.update(
+        {idNumber: hrMsg.idNumber},
+        hrMsg,
+        {upsert: true},
+        function(err) {
+            if (err) {
+                console.error('save error: \n%o', err);
+            }
+        });
+};
+
+exports.search = function(condition) {
+    return Item.find(condition).exec();
+};
+
+exports.count = {
+    insurance: 7,
+    workPlace: 7,
+    jobType: 22,
+    industry: 14,
+    preferredTraining: 15
+};
--- hrSys/routes/index.js
+++ hrSys/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var util = require('util');
+
 // for debug
 var debug = require('debug')('route');
 
 // account authentication
 var auth = require('./auth').auth;
+// access database
+var db = require('./db');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -54,8 +57,36 @@ router.get('/item', function(req, res) {
 
 /* add/modify page. */
 router.post('/item', function(req, res) {
-    //res.write('username: ' + req.body.username);
-    //res.end('IdNumber: ' + req.body.idNumber);
+    var insurance = [];
+    for (var i = 0; i < db.count.insurance; i++) {
+        if (req.body['insurance' + i]) {
+            insurance.push(i);
+        }
+    }
+    var workPlace = [];
+    for (i = 0; i < db.count.workPlace; i++) {
+        if (req.body['workPlace' + i]) {
+            workPlace.push(i);
+        }
+    }
+    var jobType = [];
+    for (i = 0; i < db.count.jobType; i++) {
+        if (req.body['jobType' + i]) {
+            jobType.push(i);
+        }
+    }
+    var industry = [];
+    for (i = 0; i < db.count.industry; i++) {
+        if (req.body['industry' + i]) {
+            industry.push(i);
+        }
+    }
+    var preferredTraining = [];
+    for (i = 0; i < db.count.preferredTraining; i++) {
+        if (req.body['preferredTraining' + i]) {
+            preferredTraining.push(i)
+        }
+    }
     var userMessage = {
         username: req.body.username,
         idNumber: req.body.idNumber,
@@ -74,91 +105,15 @@ router.post('/item', function(req, res) {
         trainingStatus: req.body.trainingStatus,
         postTraining: req.body.postTraining,
         editor: req.body.editor,
-        auditor: req.body.auditor
-    };
-    var insurance = {
-        a: req.body.insurance0,
-        b: req.body.insurance1,
-        c: req.body.insurance2,
-        d: req.body.insurance3,
-        e: req.body.insurance4,
-        f: req.body.insurance5,
-        g: req.body.insurance6
-    };
-    var workPlace = {
-        a: req.body.workPlace0,
-        b: req.body.workPlace1,
-        c: req.body.workPlace2,
-        d: req.body.workPlace3,
-        e: req.body.workPlace4,
-        f: req.body.workPlace5,
-        g: req.body.workPlace6
-    };
-    var jobType = {
-        a: req.body.jobType0,
-        b: req.body.jobType1,
-        c: req.body.jobType2,
-        d: req.body.jobType3,
-        e: req.body.jobType4,
-        f: req.body.jobType5,
-        g: req.body.jobType6,
-        h: req.body.jobType7,
-        i: req.body.jobType8,
-        j: req.body.jobType9,
-        k: req.body.jobType10,
-        l: req.body.jobType11,
-        m: req.body.jobType12,
-        n: req.body.jobType13,
-        o: req.body.jobType14,
-        p: req.body.jobType15,
-        q: req.body.jobType16,
-        r: req.body.jobType17,
-        s: req.body.jobType18,
-        t: req.body.jobType19,
-        u: req.body.jobType20,
-        v: req.body.jobType21
-    };
-    var industry = {
-        a: req.body.industry0,
-        b: req.body.industry1,
-        c: req.body.industry2,
-        d: req.body.industry3,
-        e: req.body.industry4,
-        f: req.body.industry5,
-        g: req.body.industry6,
-        h: req.body.industry7,
-        i: req.body.industry8,
-        j: req.body.industry9,
-        k: req.body.industry10,
-        l: req.body.industry11,
-        m: req.body.industry12,
-        n: req.body.industry13
-    };
-    var preferredTraining = {
-        a: req.body.preferredTraining0,
-        b: req.body.preferredTraining1,
-        c: req.body.preferredTraining2,
-        d: req.body.preferredTraining3,
-        e: req.body.preferredTraining4,
-        f: req.body.preferredTraining5,
-        g: req.body.preferredTraining6,
-        h: req.body.preferredTraining7,
-        i: req.body.preferredTraining8,
-        j: req.body.preferredTraining9,
-        k: req.body.preferredTraining10,
-        l: req.body.preferredTraining11,
-        m: req.body.preferredTraining12,
-        n: req.body.preferredTraining13,
-        o: req.body.preferredTraining14
-    };
-    res.render('editResponse', {
-        userMessage: userMessage,
+        auditor: req.body.auditor,
         insurance: insurance,
-        workPlace: workPlace,
-        jobType: jobType,
         industry: industry,
+        jobType: jobType,
+        workPlace: workPlace,
         preferredTraining: preferredTraining
-    });
+    };
+    db.save(userMessage);
+    res.render('editResponse', {userMessage: userMessage});
 });
 
 /* statistics page. */
