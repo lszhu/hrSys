3131e0212e6dbeaec2ca271e5784b6e939369cfb
Jason Zhu <jasonzhu.dev@gmail.com>
2014-06-03 15:13:35 +0800

    add search page template

--- hrSys/routes/index.js
+++ hrSys/routes/index.js
@@ -13,6 +13,17 @@ var auth = require('./auth');
 var db = require('./db');
 // district Id and name
 var districtName = require('../config/districtId');
+// nations
+var nations = [
+    "汉族","蒙古族","回族","藏族","维吾尔族","苗族","彝族","壮族",
+    "布依族","朝鲜族","满族","侗族","瑶族","白族","土家族","哈尼族",
+    "哈萨克族","傣族","黎族","傈傈族","佤族","畲族","高山族","拉祜族",
+    "水族","东乡族","纳西族","景颇族","柯尔克孜族","土族","达翰尔族",
+    "仫佬族","羌族","布朗族","撒拉族","毛南族","仡佬族","锡伯族",
+    "阿昌族","普米族","塔吉克族","怒族","乌孜别克族","俄罗斯族",
+    "鄂温克族","德昂族","保安族","裕固族","京族","塔塔尔族","独龙族",
+    "鄂伦春族","赫哲族","门巴族","珞巴族","基诺族","外籍人士"
+];
 
 function getAdminAreaName(districtId) {
     if (districtId == '0') {
@@ -71,7 +82,7 @@ router.post('/login', function(req, res) {
 
 });
 
-/* administration page. */
+/* independent account management page. */
 router.get('/account', function(req, res) {
     db.queryAccounts(
         {type: 'independent'},
@@ -121,7 +132,7 @@ router.get('/account', function(req, res) {
     );
 });
 
-/* administration page. */
+/* independent account management page. */
 router.post('/account', function(req, res) {
     if (req.body.password != req.body.retryPassword) {
         console.log('password not match');
@@ -292,7 +303,8 @@ router.post('/batchAccount', function(req, res) {
 router.get('/item', function(req, res) {
     res.render('item', {
         title: 'add/modify item',
-        adminArea: getAdminAreaName(req.session.user.area)
+        adminArea: getAdminAreaName(req.session.user.area),
+        nations: nations
     });
 });
 
@@ -464,7 +476,9 @@ router.get('/search', function(req, res) {
         'search',
         {
             title: 'search',
-            adminArea: getAdminAreaName(req.session.user.area)
+            adminArea: getAdminAreaName(req.session.user.area),
+            nations: nations,
+            districtName: districtName
         }
     );
 });
@@ -475,7 +489,8 @@ router.post('/search', function(req, res) {
         'search',
         {
             title: 'search',
-            adminArea: getAdminAreaName(req.session.user.area)
+            adminArea: getAdminAreaName(req.session.user.area),
+            nations: nations
         }
     );
 });
