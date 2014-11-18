bd86353619c989aaf6552f3dc07569c92b311b12
Jason Zhu <jasonzhu.dev@gmail.com>
2014-11-18 22:48:11 +0800

    tuned for deployment

--- deploy.win/backup.bat
+++ deploy.win/backup.bat
@@ -2,3 +2,4 @@
 "C:\Program Files\MongoDB 2.6 Standard\bin\mongodump.exe" -u admin -p letmein --authenticationDatabase admin -d ningyuan -o D:\hrSys\dataBackup
 "C:\Program Files\MongoDB 2.6 Standard\bin\mongodump.exe" -u admin -p letmein --authenticationDatabase admin -d xintian -o D:\hrSys\dataBackup
 "C:\Program Files\MongoDB 2.6 Standard\bin\mongodump.exe" -u admin -p letmein --authenticationDatabase admin -d lengshuitan -o D:\hrSys\dataBackup
+"C:\Program Files\MongoDB 2.6 Standard\bin\mongodump.exe" -u admin -p letmein --authenticationDatabase admin -d qiyang -o D:\hrSys\dataBackup
